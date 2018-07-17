1f5c29138a6815ccfa115de69823fcde0cd37b6e
Ayodele Opetumo <aopetumo@gmail.com>
2018-07-17 16:47:53 +0100

    Clean up files for dynamic component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,12 @@ import { User } from './auth-form/auth-form.interface';
   styleUrls: ['./app.component.css'],
   template: `
     <div>
-      <!-- <auth-form (submitted)="createUser($event)">
-        <h3>Create account</h3>
-        <button type="submit">Join Us</button>
-      </auth-form> -->
-      <auth-form (submitted)="loginUser($event)">
-        <button type="submit">Login</button>
-        <auth-remember (checked)="rememberUser($event)"></auth-remember>
-        <h3>Login</h3>
-      </auth-form>
+      <auth-form></auth-form>
     </div>
   `
 })
 export class AppComponent {
-  rememberMe = false;
-
-  rememberUser(remember: boolean) {
-    this.rememberMe = remember;
-  }
-
-  createUser(user: User) {
-    console.log('Create account', user);
-  }
-
   loginUser(user: User) {
-    console.log('Login', user, this.rememberMe);
+    console.log('Login', user);
   }
 }
--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,21 +1,4 @@
-import {
-  Component,
-  Output,
-  EventEmitter,
-  AfterContentInit,
-  ContentChild,
-  ContentChildren,
-  QueryList,
-  ViewChild,
-  AfterViewInit,
-  ViewChildren,
-  ChangeDetectorRef,
-  ElementRef,
-  Renderer2
-} from '@angular/core';
-
-import { AuthRememberComponent } from './auth-remember.component';
-import { AuthMessageComponent } from './auth-message.component';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 import { User } from './auth-form.interface';
 
@@ -32,7 +15,7 @@ import { User } from './auth-form.interface';
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-        <ng-content select="h3"></ng-content>
+        <h3>{{ title }}</h3>
         <label>
           Email address
           <input type="email" name="email" ngModelc #email>
@@ -41,66 +24,17 @@ import { User } from './auth-form.interface';
           Password
           <input type="password" name="password" ngModel>
         </label>
-        <!-- The select attribute works just like the querySelector function;
-        by passing in tag, class or id selectors -->
-        <ng-content select="auth-remember"></ng-content>
-        <auth-message [style.display]="(showMessage ? 'inherit' : 'none')"></auth-message>
-        <ng-content select="button"></ng-content>
+        <button>{{ title }}</button>
       </form>
     </div>
   `
 })
-export class AuthFormComponent implements AfterContentInit, AfterViewInit {
-  showMessage: boolean;
-
-  @ViewChild('email') emailRef: ElementRef;
-  @ViewChild(AuthMessageComponent) message: AuthMessageComponent;
-  @ViewChildren(AuthMessageComponent) messages: QueryList<AuthMessageComponent>;
-
-  @ContentChildren(AuthRememberComponent)
-  remember: QueryList<AuthRememberComponent>;
+export class AuthFormComponent {
+  title = 'Login';
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
-  constructor(private db: ChangeDetectorRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    // Platform agnostic DOM ref API
-    this.renderer.setAttribute(
-      this.emailRef.nativeElement,
-      'placeholder',
-      'Enter your email address'
-    );
-    this.renderer.addClass(this.emailRef.nativeElement, 'email');
-    this.emailRef.nativeElement.focus();
-
-    /*
-      Web specific DOM ref API
-
-    this.emailRef.nativeElement.setAttribute(
-      'placeholder',
-      'Enter email address'
-    );
-    this.emailRef.nativeElement.classList.add('email');
-    this.emailRef.nativeElement.focus(); */
-
-    /*
-       Reference for the ViewChildren decorator is only available in this lifecycle hook,
-       but must also implement a ChangeDetector
-    */
-    this.messages.forEach(message => (message.days = 30));
-    this.db.detectChanges();
-  }
-
-  ngAfterContentInit() {
-    // this.message.numberOfDays = 30;
-
-    if (this.remember) {
-      this.remember.forEach(item =>
-        item.checked.subscribe(checked => (this.showMessage = checked))
-      );
-    }
-  }
+  constructor() {}
 
   onSubmit(value: User) {
     this.submitted.emit(value);
